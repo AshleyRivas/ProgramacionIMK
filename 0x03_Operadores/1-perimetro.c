3e0c3ba7f7f1655789c16f93456c8390726ea12b
AshleyRivas <112293497+AshleyRivas@users.noreply.github.com>
2022-09-26 16:54:04 -0500

    Delete 0x03_Operadores directory

--- 0x03_Operadores/1-perimetro.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-//formula para calcular perimetro de un rectangulo
-
-float perimetro (float largo, float ancho)
-{
-    return 2 * (largo + ancho);
-}
--- 0x03_Operadores/2-area.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-//formula para calcular area de un rectangulo
-
-float area (float largo, float ancho)
-{
-    return (largo * ancho);
-}
