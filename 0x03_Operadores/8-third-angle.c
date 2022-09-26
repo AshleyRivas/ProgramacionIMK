4e615218aaf1cfa2b9015649c79db061f4691e6d
AshleyRivas <112293497+AshleyRivas@users.noreply.github.com>
2022-09-26 12:54:10 -0500

    Add files via upload

---
+++ 0x03_Operadores/8-third-angle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main ()
+{
+    int a, b, c;
+    c=180- (a + b);
+
+    printf ("Ingrese dos angulos del triangulo: \n");
+    scanf ("%d %d", &a, &b);
+
+    printf ("El angulo es: %d \n",c);
+    return 0;
+}
